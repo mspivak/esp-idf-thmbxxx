0d8cb1ea6276a88cc2c51acd12e34bd5b38c90a8
Mauro Spivak <mauros@gmail.com>
2024-03-03 22:40:25 -0300

    first revision, working

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Slicetext Modbus RTU Master Example
 
-This example inherits from https://github.com/espressif/esp-idf/blob/master/examples/protocols/modbus/serial/mb_master and shows how to read all 6 characteristics from TH-MB-XXX-DS sensors from [Slicetex](https://slicetex.com/).
+This example inherits from https://github.com/espressif/esp-idf/blob/master/examples/protocols/modbus/serial/mb_master.
+
+Shows how to read all 6 characteristics from TH-MB-XXX-DS sensors from [Slicetex](https://slicetex.com/).
+
+Outputs:
+
+    I (555) modbus_example: Characteristic #0 Humidity: 31.9 %
+    I (1595) modbus_example: Characteristic #1 Temperature: 27.1 C
+    I (2635) modbus_example: Characteristic #2 Humidity calibration: 0.0 %
+    I (3675) modbus_example: Characteristic #3 Temperature Calibration: 0.0 C
+    I (4715) modbus_example: Characteristic #4 ModBus address: 1
+    I (5755) modbus_example: Characteristic #5 ModBus baud rate: 4800 bauds
--- main/master.c
+++ main/master.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2016-2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2016-2023 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -19,16 +19,13 @@
 // The number of parameters that intended to be used in the particular control process
 #define MASTER_MAX_CIDS num_device_parameters
 
-// Number of reading of parameters from slave
-#define MASTER_MAX_RETRY 3000
-
 // Timeout to update cid over Modbus
-#define UPDATE_CIDS_TIMEOUT_MS (5000)
-#define UPDATE_CIDS_TIMEOUT_TICS (UPDATE_CIDS_TIMEOUT_MS / portTICK_PERIOD_MS)
 
-// Timeout between polls
-#define POLL_TIMEOUT_MS (500)
-#define POLL_TIMEOUT_TICS (POLL_TIMEOUT_MS / portTICK_PERIOD_MS)
+#define SECOND (1000 / portTICK_PERIOD_MS)
+#define MINUTE (60 * SECOND)
+
+#define TIME_BETWEEN_CHARS (1 * SECOND)
+#define TIME_BETWEEN_READINGS (1 * SECOND)
 
 // The macro to get offset for parameter in the appropriate structure
 #define HOLD_OFFSET(field) ((uint16_t)(offsetof(holding_reg_params_t, field) + 1))
@@ -38,13 +35,15 @@
 #define DISCR_OFFSET(field) ((uint16_t)(offsetof(discrete_reg_params_t, field) + 1))
 
 #define STR(fieldname) ((const char *)(fieldname))
-
+// Options can be used as bit masks or parameter limits
 #define OPTS(min_val, max_val, step_val)                   \
     {                                                      \
         .opt1 = min_val, .opt2 = max_val, .opt3 = step_val \
     }
 
-static const char *TAG = "app_modbus";
+static const char *TAG = "modbus_example";
+
+const int BAUDRATE_MAP[] = {2400, 4800, 9600};
 
 // Enumeration of modbus device addresses accessed by master device
 enum
@@ -55,7 +54,7 @@ enum
 // Enumeration of all supported CIDs for device (used in parameter definition table)
 enum
 {
-    CIS_HUM = 0,
+    CID_HUM = 0,
     CID_TEMP,
     CID_CAL_HUM,
     CID_CAL_TEMP,
@@ -63,117 +62,188 @@ enum
     CID_BAUD,
 };
 
-esp_err_t modbus_read_characteristic(const mb_parameter_descriptor_t *param_descriptor, uint16_t *value)
+// Example Data (Object) Dictionary for Modbus parameters:
+// The CID field in the table must be unique.
+// Modbus Slave Addr field defines slave address of the device with correspond parameter.
+// Modbus Reg Type - Type of Modbus register area (Holding register, Input Register and such).
+// Reg Start field defines the start Modbus register number and Reg Size defines the number of registers for the characteristic accordingly.
+// The Instance Offset defines offset in the appropriate parameter structure that will be used as instance to save parameter value.
+// Data Type, Data Size specify type of the characteristic and its data size.
+// Parameter Options field specifies the options that can be used to process parameter value (limits or masks).
+// Access Mode - can be used to implement custom options for processing of characteristic (Read/Write restrictions, factory mode values and etc).
+const mb_parameter_descriptor_t device_parameters[] = {
+    // { CID, Param Name, Units, Modbus Slave Addr, Modbus Reg Type, Reg Start, Reg Size, Instance Offset, Data Type, Data Size, Parameter Options, Access Mode }
+    {CID_HUM, STR("Humidity"), STR("%"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 0, 1, HOLD_OFFSET(holding_data0), PARAM_TYPE_U16, 2, {}, PAR_PERMS_READ},
+    {CID_TEMP, STR("Temperature"), STR("C"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 1, 1, HOLD_OFFSET(holding_data1), PARAM_TYPE_U16, 2, {}, PAR_PERMS_READ},
+    {CID_CAL_HUM, STR("Humidity calibration"), STR("%"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 80, 1, HOLD_OFFSET(holding_data2), PARAM_TYPE_U16, 2, OPTS(-100, 100, 1), PAR_PERMS_READ},
+    {CID_CAL_TEMP, STR("Temperature Calibration"), STR("C"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 81, 1, HOLD_OFFSET(holding_data3), PARAM_TYPE_U16, 2, OPTS(-100, 100, 1), PAR_PERMS_READ},
+    {CID_ADDR, STR("ModBus address"), STR(""), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 2000, 1, HOLD_OFFSET(holding_data4), PARAM_TYPE_U8, 1, OPTS(0, 100, 1), PAR_PERMS_READ},
+    {CID_BAUD, STR("ModBus baud rate"), STR("bauds"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 2001, 1, HOLD_OFFSET(holding_data5), PARAM_TYPE_U8, 1, OPTS(0, 10000, 1), PAR_PERMS_READ},
+};
+
+// Calculate number of parameters in the table
+const uint16_t num_device_parameters = (sizeof(device_parameters) / sizeof(device_parameters[0]));
+
+// The function to get pointer to parameter storage (instance) according to parameter description table
+static void *master_get_param_data(const mb_parameter_descriptor_t *param_descriptor)
 {
-    esp_err_t err = ESP_OK;
-
-    // Get data from parameters description table
-    // and use this information to fill the characteristics description table
-    // and having all required fields in just one table
-    err = mbc_master_get_cid_info(param_descriptor->cid, &param_descriptor);
-    if ((err != ESP_ERR_NOT_FOUND) && (param_descriptor != NULL))
+    assert(param_descriptor != NULL);
+    void *instance_ptr = NULL;
+    if (param_descriptor->param_offset != 0)
     {
-        uint8_t type = 0;
-
-        err = mbc_master_get_parameter(param_descriptor->cid, (char *)param_descriptor->param_key,
-                                       (uint8_t *)value, &type);
-        if (err != ESP_OK)
+        switch (param_descriptor->mb_param_type)
         {
-            ESP_LOGE(TAG, "Characteristic #%d (%s) read fail, err = 0x%x (%s).",
-                     param_descriptor->cid,
-                     (char *)param_descriptor->param_key,
-                     (int)err,
-                     (char *)esp_err_to_name(err));
+        case MB_PARAM_HOLDING:
+            instance_ptr = ((void *)&holding_reg_params + param_descriptor->param_offset - 1);
+            break;
+        case MB_PARAM_INPUT:
+            instance_ptr = ((void *)&input_reg_params + param_descriptor->param_offset - 1);
+            break;
+        case MB_PARAM_COIL:
+            instance_ptr = ((void *)&coil_reg_params + param_descriptor->param_offset - 1);
+            break;
+        case MB_PARAM_DISCRETE:
+            instance_ptr = ((void *)&discrete_reg_params + param_descriptor->param_offset - 1);
+            break;
+        default:
+            instance_ptr = NULL;
+            break;
         }
-
-        ESP_LOGI(TAG, "Characteristic #%d - %s = %0.1f %s read successful.",
-                 param_descriptor->cid,
-                 (char *)param_descriptor->param_key,
-                 (float)*value / 10,
-                 (char *)param_descriptor->param_units);
-
-        vTaskDelay(POLL_TIMEOUT_TICS); // timeout between polls
     }
-    return err;
+    else
+    {
+        ESP_LOGE(TAG, "Wrong parameter offset for CID #%u", (unsigned)param_descriptor->cid);
+        assert(instance_ptr != NULL);
+    }
+    return instance_ptr;
 }
 
 // User operation function to read slave values and check alarm
-static void modbus_read_sensor(void)
+static void master_operation_func(void *arg)
 {
+    esp_err_t err = ESP_OK;
+    u_int16_t value = 0;
+    const mb_parameter_descriptor_t *param_descriptor = NULL;
 
-    uint16_t hum = 0;
-    uint16_t temp = 0;
-    mb_parameter_descriptor_t hum_descriptor = {CIS_HUM, STR("Humidity"), STR("%"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 0, 1, HOLD_OFFSET(holding_data0), PARAM_TYPE_U16, 2, {}, PAR_PERMS_READ};
-    mb_parameter_descriptor_t temp_descriptor = {CID_TEMP, STR("Temperature"), STR("C"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 1, 1, HOLD_OFFSET(holding_data1), PARAM_TYPE_U16, 2, {}, PAR_PERMS_READ};
+    ESP_LOGI(TAG, "Start modbus test...");
 
-    // {CID_CAL_HUM, STR("Humidity calibration"), STR("%"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 80, 1, HOLD_OFFSET(holding_data2), PARAM_TYPE_U16, 2, OPTS(-100, 100, 1), PAR_PERMS_READ},
-    // {CID_CAL_TEMP, STR("Temperature Calibration"), STR("C"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 81, 1, HOLD_OFFSET(holding_data3), PARAM_TYPE_U16, 2, OPTS(-100, 100, 1), PAR_PERMS_READ},
-    // {CID_ADDR, STR("ModBus address"), STR(""), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 2000, 1, HOLD_OFFSET(holding_data4), PARAM_TYPE_U8, 1, OPTS(0, 100, 1), PAR_PERMS_READ},
-    // {CID_BAUD, STR("ModBus baud rate"), STR("Bauds"), MB_DEVICE_ADDR1, MB_PARAM_HOLDING, 2001, 1, HOLD_OFFSET(holding_data5), PARAM_TYPE_U8, 1, OPTS(0, 10000, 1), PAR_PERMS_READ},
+    while (true)
+    {
+        // Read all found characteristics from slave(s)
+        for (uint16_t cid = 0; (err != ESP_ERR_NOT_FOUND) && cid < MASTER_MAX_CIDS; cid++)
+        {
+            // Get data from parameters description table
+            // and use this information to fill the characteristics description table
+            // and having all required fields in just one table
+            err = mbc_master_get_cid_info(cid, &param_descriptor);
+            if ((err != ESP_ERR_NOT_FOUND) && (param_descriptor != NULL))
+            {
+                void *temp_data_ptr = master_get_param_data(param_descriptor);
+                assert(temp_data_ptr);
+                uint8_t type = 0;
+                err = mbc_master_get_parameter(cid,
+                                               (char *)param_descriptor->param_key,
+                                               (uint8_t *)temp_data_ptr, &type);
+                if (err != ESP_OK)
+                {
 
-    ESP_LOGI(TAG, "Reading Modbus sensor.");
+                    ESP_LOGE(TAG, "Characteristic #%u %s read fail, err 0x%x: %s.",
+                             param_descriptor->cid,
+                             param_descriptor->param_key,
+                             (int)err,
+                             (char *)esp_err_to_name(err));
 
-    ESP_ERROR_CHECK(modbus_read_characteristic(&hum_descriptor, &hum));
-    ESP_ERROR_CHECK(modbus_read_characteristic(&temp_descriptor, &temp));
+                    continue;
+                }
+
+                value = *(u_int16_t *)temp_data_ptr;
+
+                if (param_descriptor->cid == CID_ADDR || param_descriptor->cid == CID_BAUD)
+                {
+
+                    if (param_descriptor->cid == CID_BAUD)
+                    {
+                        value = BAUDRATE_MAP[value];
+                    }
+
+                    ESP_LOGI(TAG, "Characteristic #%u %s: %d %s",
+                             param_descriptor->cid,
+                             param_descriptor->param_key,
+                             value,
+                             param_descriptor->param_units);
+                }
+                else
+                {
+
+                    ESP_LOGI(TAG, "Characteristic #%u %s: %.1f %s",
+                             param_descriptor->cid,
+                             param_descriptor->param_key,
+                             (float)value / 10,
+                             param_descriptor->param_units);
+                }
+            }
+            vTaskDelay(TIME_BETWEEN_CHARS);
+        }
+        vTaskDelay(TIME_BETWEEN_READINGS);
+    }
+
+    ESP_LOGI(TAG, "Destroy master...");
+    ESP_ERROR_CHECK(mbc_master_destroy());
 }
 
-static esp_err_t modbus_master_init(void)
+// Modbus master initialization
+static esp_err_t master_init(void)
 {
+    // Initialize and start Modbus controller
     mb_communication_info_t comm = {
         .port = MB_PORT_NUM,
+#if CONFIG_MB_COMM_MODE_ASCII
+        .mode = MB_MODE_ASCII,
+#elif CONFIG_MB_COMM_MODE_RTU
         .mode = MB_MODE_RTU,
+#endif
         .baudrate = MB_DEV_SPEED,
-        .parity = MB_PARITY_NONE};
+        .parity = MB_PARITY_NONE
+    };
     void *master_handler = NULL;
 
     esp_err_t err = mbc_master_init(MB_PORT_SERIAL_MASTER, &master_handler);
     MB_RETURN_ON_FALSE((master_handler != NULL), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus controller initialization fail.");
+                       "mb controller initialization fail.");
     MB_RETURN_ON_FALSE((err == ESP_OK), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus controller initialization fail, returns(0x%x).",
-                       (uint32_t)err);
+                       "mb controller initialization fail, returns(0x%x).", (int)err);
     err = mbc_master_setup((void *)&comm);
     MB_RETURN_ON_FALSE((err == ESP_OK), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus controller setup fail, returns(0x%x).",
-                       (uint32_t)err);
+                       "mb controller setup fail, returns(0x%x).", (int)err);
 
     // Set UART pin numbers
     err = uart_set_pin(MB_PORT_NUM, CONFIG_MB_UART_TXD, CONFIG_MB_UART_RXD,
                        CONFIG_MB_UART_RTS, UART_PIN_NO_CHANGE);
     MB_RETURN_ON_FALSE((err == ESP_OK), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus serial set pin failure, uart_set_pin() returned (0x%x).", (uint32_t)err);
+                       "mb serial set pin failure, uart_set_pin() returned (0x%x).", (int)err);
 
     err = mbc_master_start();
     MB_RETURN_ON_FALSE((err == ESP_OK), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus controller start fail, returns(0x%x).",
-                       (uint32_t)err);
+                       "mb controller start fail, returned (0x%x).", (int)err);
 
     // Set driver mode to Half Duplex
     err = uart_set_mode(MB_PORT_NUM, UART_MODE_RS485_HALF_DUPLEX);
     MB_RETURN_ON_FALSE((err == ESP_OK), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus serial set mode failure, uart_set_mode() returned (0x%x).", (uint32_t)err);
+                       "mb serial set mode failure, uart_set_mode() returned (0x%x).", (int)err);
 
     vTaskDelay(5);
     err = mbc_master_set_descriptor(&device_parameters[0], num_device_parameters);
     MB_RETURN_ON_FALSE((err == ESP_OK), ESP_ERR_INVALID_STATE, TAG,
-                       "ModBus controller set descriptor fail, returns(0x%x).",
-                       (uint32_t)err);
-
-    ESP_LOGI(TAG, "Modbus master stack initialized.");
+                       "mb controller set descriptor fail, returns(0x%x).", (int)err);
+    ESP_LOGI(TAG, "Modbus master stack initialized...");
     return err;
 }
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(modbus_master_init());
+    // Initialization of device peripheral and objects
+    ESP_ERROR_CHECK(master_init());
     vTaskDelay(10);
 
-    while (true)
-    {
-        modbus_read_sensor();
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-
-    ESP_LOGI(TAG, "Destroying master modbus handle.");
-    ESP_ERROR_CHECK(mbc_master_destroy());
+    master_operation_func(NULL);
 }
